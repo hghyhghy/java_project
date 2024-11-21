c38db3c6d957b4f5dbac87a337875a7bb6952ae6
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2024-11-21 13:13:34 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+public class Pattern{
+
+	public static void main(String[] args) {
+		
+		int n=5;
+ 		for (int i=1;i<=n;i++){ // Outer loop for rows
+
+		    for (int j=1; j<=i; j++){ //  inner  loop for columns
+
+			System.out.println(i + " ");// printing the number
+                                         }
+                                     System.out.println() ; // for moving to the next line 		
+		}
+
+	}
+
+}
+
+
